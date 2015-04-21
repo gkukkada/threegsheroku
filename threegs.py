9eb76ab1ecbcd140e545d121346285062eadcc02
Raja Simon <g33ksimon@gmail.com>
2015-04-22 02:18:58 +0530

    threegs first commit

---
+++ threegs.py
@@ -0,0 +1,16 @@
+# Python
+import os 
+from pprint import pprint
+
+# Flask
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+	if request.method == 'POST':
+		pprint(request.form['hashtag'])
+	return render_template('index.html')
+
+if __name__ == '__main__':
+	app.run(debug=True)
