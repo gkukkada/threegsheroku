bbd336f90d56e86532d26907032f52c82729477a
Raja Simon <g33ksimon@gmail.com>
2015-04-22 08:16:16 +0530

    template all up

--- threegs.py
+++ threegs.py
@@ -3,14 +3,29 @@ import os
 from pprint import pprint
 
 # Flask
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
+
+# set the secret key.  keep this really secret:
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
+	if 'hashtag' in session:
+		session.pop('hashtag', None)
+
 	if request.method == 'POST':
-		pprint(request.form['hashtag'])
-	return render_template('index.html')
+		""" get the form data using request form"""
+		session['hashtag'] = request.form['hashtag']
+		return redirect(url_for('map_view'))
+	return render_template('handle/home.html')
+
+@app.route('/map/')
+def map_view():
+	if 'hashtag' in session:
+		return render_template('handle/map.html')
+	else:
+		return redirect(url_for('index'))
 
 if __name__ == '__main__':
 	app.run(debug=True)
