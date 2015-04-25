89fcb41540f0b72175dc33707168765ec287066a
Gowtham Kukkadapu <gkukkada@kent.edu>
2015-04-25 19:11:15 -0400

    changed hashtag in pthreegs.py

--- threegs.py
+++ threegs.py
@@ -38,7 +38,7 @@ def map():
 	if hashtag[0] == '#':
 		track = hashtag
 	else:
-		track = ['#%s'.format(hashtag)]
+		track = ['{}'.format(hashtag)]
 		#track = ['hashtag']
 
 	""" gonna starts here """
