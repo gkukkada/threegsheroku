a23c21d17d1cdf5612fe8e079239780229126a56
Gowtham Kukkadapu <gkukkada@kent.edu>
2015-04-25 18:49:52 -0400

    changed hashtag in pthreegs.py

--- threegs.py
+++ threegs.py
@@ -38,7 +38,8 @@ def map():
 	if hashtag[0] == '#':
 		track = hashtag
 	else:
-		track = ['#{}'.format(hashtag)]
+		#track = ['#{}'.format(hashtag)]
+		track = hashtag
 
 	""" gonna starts here """
 	session['random_userid'] = randint(1, 999)
